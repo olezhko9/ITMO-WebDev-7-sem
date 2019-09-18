733d4c15e85e6be18059f23ac6f1260f10470e1d
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-09-18 03:12:14 +0300

    update weather layout

--- lab_1/src/js/main.js
+++ lab_1/src/js/main.js
@@ -3,10 +3,14 @@ import renderWeather from '../components/weather'
 
 $(function () {
   $('#search-btn').click(async function () {
+
     const weatherData = await getWeather($('#city-input').val())
-    const html = renderWeather({ name: weatherData.location.name })
-    const div = document.createElement("div");
-    div.innerHTML = html;
-    document.body.appendChild(div);
+    const html = renderWeather({
+      city: weatherData.location.name,
+      weather: weatherData.current
+    })
+
+    const div = $('<div></div>').html(html);
+    $('body').append(div)
   })
 })
