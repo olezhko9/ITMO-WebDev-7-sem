5ece065a5b1af495f7dbf5f536c3d088268a9a30
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-09-19 01:28:53 +0300

    removed redundant pug template compilation

--- lab_1/src/js/main.js
+++ lab_1/src/js/main.js
@@ -1,13 +1,15 @@
 import getWeather from './weather'
-import renderWeather from '../components/weather'
+
+const template = require('../components/weather.pug')
 
 $(function () {
   $('#search-btn').click(async function () {
 
     const weatherData = await getWeather($('#city-input').val())
-    const html = renderWeather({
-      city: weatherData.location.name,
-      weather: weatherData.current
+
+    const html = template({
+        city: weatherData.location.name,
+        weather: weatherData.current
     })
 
     const div = $('<div></div>').html(html);
