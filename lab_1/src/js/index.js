4f82db4fd770a32ba38eef0f7f7f25a94a6af1ba
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-09-23 16:43:02 +0300

    minor fixes

--- lab_1/src/js/index.js
+++ lab_1/src/js/index.js
@@ -1,24 +1,22 @@
 import getWeather from './weather'
 import '../style/main.sass'
-const template = require('../components/weather.pug')
+const weatherTemplate = require('../components/weather.pug')
 
 $(function () {
   $('#search-btn').click(async function () {
     $('#search-btn').addClass('loading')
     const weatherData = await getWeather($('#city-input').val())
 
-    const html = template({
+    const weatherComponent = weatherTemplate({
       city: weatherData.location.name,
       weather: weatherData.current
     })
 
-    const div = $('<div></div>').html(html);
-    $('.weather-main').append(div)
+    $('.weather-main').append(weatherComponent)
     $('#search-btn').removeClass('loading')
   })
 
   $(document).on('click', '.close', function (e) {
-    console.log(e);
-    $('.weather-component').remove()
+    $(this).parent().remove()
   })
 })
