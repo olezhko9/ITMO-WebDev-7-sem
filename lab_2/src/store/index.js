996e915aac5b39884fee58743bc294c603eb2c39
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-10-09 00:02:32 +0300

    save and load Redux state to and from LocalStorage

--- lab_2/src/store/index.js
+++ lab_2/src/store/index.js
@@ -1,7 +1,16 @@
 import {createStore} from "redux";
 import reducer from "./reducer";
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__());
+const STORAGE_KEY = 'weather-cities'
+const preloadedState = localStorage[STORAGE_KEY] ? JSON.parse(localStorage[STORAGE_KEY]) : []
+
+const store = createStore(
+  reducer,
+  preloadedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => {
+  localStorage[STORAGE_KEY] = JSON.stringify(store.getState())
+})
 
 export default store;
