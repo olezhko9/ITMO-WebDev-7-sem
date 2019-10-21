3347b12c8f49457b4c5d7bc4f8ea8414725065b3
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-10-21 20:28:20 +0300

    added favorite cities cintainer component

--- lab_2/src/components/App/index.js
+++ lab_2/src/components/App/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Container, Grid, Button, TextField, Fab, createMuiTheme, MuiThemeProvider} from '@material-ui/core'
-import AddIcon from '@material-ui/icons/Add';
+
+import {Container, Grid, Button, createMuiTheme, MuiThemeProvider} from '@material-ui/core'
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import CurrentWeather from "../CurrentWeather";
 import Typography from "@material-ui/core/Typography";
 
-import {connect} from 'react-redux';
-import {addCity, removeCity} from "../../store/actions";
+import CurrentWeather from "../CurrentWeather";
+import FavoriteCitiesContainer from "../FavoriteCitiesContainer";
 
 const useStyles = makeStyles(theme => ({
   fab: {
@@ -33,7 +32,6 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      enteredCity: '',
       currentLocation: ''
     }
   }
@@ -65,20 +63,7 @@ class App extends React.Component {
     }
   }
 
-  addCityToFavorite() {
-    if (this.state.enteredCity !== "")
-      this.props.addCity(this.state.enteredCity)
-  }
 
-  removeCityFromFavorite(cityName) {
-    console.log(cityName)
-    this.props.removeCity(cityName)
-  }
-
-  onCityInput(e) {
-    const city = e.target.value
-    this.setState({enteredCity: city})
-  }
 
   render() {
     // const styles = useStyles()
@@ -98,36 +83,9 @@ class App extends React.Component {
             </Grid>
           </Grid>
 
-          <Grid container spacing={1} component={"section"}>
-            <CurrentWeather key={"default"} location={this.state.currentLocation} isFavorite={false}/>
-          </Grid>
+          <CurrentWeather key={"default"} location={this.state.currentLocation} isFavorite={false}/>
 
-          <Grid container component={"section"} alignItems={"center"}>
-            <Grid item xs={12} sm={5}>
-              <Typography variant="h4" component="h2">
-                Избранное
-              </Typography>
-            </Grid>
-            <Grid container item xs={12} sm={7} className={"addCityInput"}>
-              <TextField
-                label="Добавить новый город"
-                placeholder="Город"
-                margin={"none"}
-                onChange={this.onCityInput.bind(this)}
-              />
-              <Fab size="small" color="primary" onClick={this.addCityToFavorite.bind(this)}>
-                <AddIcon/>
-              </Fab>
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={4} component={"section"}>
-            {this.props.cities.map((city, index) =>
-              <Grid key={index} item xs={12} md={6}>
-                <CurrentWeather location={city} isFavorite onRemoveCityClick={this.removeCityFromFavorite.bind(this)}/>
-              </Grid>
-            )}
-          </Grid>
+          <FavoriteCitiesContainer />
 
         </Container>
       </MuiThemeProvider>
@@ -135,13 +93,4 @@ class App extends React.Component {
   }
 }
 
-export default connect(
-  state => ({
-    cities: state
-  }),
-  {
-    addCity,
-    removeCity
-  }
-)(App);
-
+export default App;
---
+++ lab_2/src/components/FavoriteCitiesContainer/index.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+import {Fab, Grid, TextField} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import AddIcon from "@material-ui/core/SvgIcon/SvgIcon";
+
+import {addCity, removeCity} from "../../store/actions";
+import {connect} from 'react-redux';
+
+import CurrentWeather from "../CurrentWeather";
+
+
+class FavoriteCities extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      enteredCity: ''
+    }
+  }
+
+  addCityToFavorite() {
+    if (this.state.enteredCity !== "")
+      this.props.addCity(this.state.enteredCity)
+  }
+
+  removeCityFromFavorite(cityName) {
+    this.props.removeCity(cityName)
+  }
+
+  onCityInput(e) {
+    const city = e.target.value
+    this.setState({enteredCity: city})
+  }
+
+  render() {
+    return (
+      <Grid>
+
+        <Grid container component={"section"} alignItems={"center"}>
+          <Grid item xs={12} sm={5}>
+            <Typography variant="h4" component="h2">
+              Избранное
+            </Typography>
+          </Grid>
+          <Grid container item xs={12} sm={7} className={"addCityInput"}>
+            <TextField
+              label="Добавить новый город"
+              placeholder="Город"
+              margin={"none"}
+              onChange={this.onCityInput.bind(this)}
+            />
+            <Fab size="small" color="primary" onClick={this.addCityToFavorite.bind(this)}>
+              <AddIcon/>
+            </Fab>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={4} component={"section"}>
+          {this.props.cities.map((city, index) =>
+            <Grid key={index} item xs={12} md={6}>
+              <CurrentWeather location={city} isFavorite onRemoveCityClick={this.removeCityFromFavorite.bind(this)}/>
+            </Grid>
+          )}
+        </Grid>
+
+      </Grid>
+    )
+  }
+}
+
+export default connect(
+  state => ({
+    cities: state
+  }),
+  {
+    addCity,
+    removeCity
+  }
+)(FavoriteCities);
+
