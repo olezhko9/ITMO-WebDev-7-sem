d7a7ee2f199d5f51932ba072078241d4fe89b8b6
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-10-23 01:27:36 +0300

    added weather service

---
+++ lab_2/src/services/weather.js
@@ -0,0 +1,13 @@
+export function fetchWeather(location) {
+  let url = ''
+
+  if (Array.isArray(location)) {
+    url = `http://api.openweathermap.org/data/2.5/weather?lat=${location[0]}&lon=${location[1]}&appid=263bacc60191ddc5e17b82d2d0c753d4`
+  } else if (typeof location === 'string' && location.length) {
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=263bacc60191ddc5e17b82d2d0c753d4`
+  } else return
+
+  return fetch(url)
+    .then(res => res.json())
+    .catch(e => console.log)
+}
--- lab_2/src/store/actions.js
+++ lab_2/src/store/actions.js
@@ -1,10 +1,4 @@
-
-function fetchWeather(location) {
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=263bacc60191ddc5e17b82d2d0c753d4`
-  return fetch(url)
-    .then(res => res.json())
-    .catch(e => console.log)
-}
+import {fetchWeather} from "../services/weather";
 
 export const addCity = (city) => async dispatch => {
   const weather = await fetchWeather(city);
