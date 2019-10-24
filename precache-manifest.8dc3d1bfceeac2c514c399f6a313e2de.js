159901c84f283c7836c3b87ae8d5a58c2f761773
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-10-25 02:43:15 +0300

    Updates

---
+++ precache-manifest.8dc3d1bfceeac2c514c399f6a313e2de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463d7ba8a4cc52ce450bfbf9756767d4",
+    "url": "/ITMO-WebDev-7-sem/lab_2/index.html"
+  },
+  {
+    "revision": "65c2e83dcf4cb1295a8d",
+    "url": "/ITMO-WebDev-7-sem/lab_2/static/css/main.9a498bcb.chunk.css"
+  },
+  {
+    "revision": "5274795a986f8d351040",
+    "url": "/ITMO-WebDev-7-sem/lab_2/static/js/2.f49245bf.chunk.js"
+  },
+  {
+    "revision": "65c2e83dcf4cb1295a8d",
+    "url": "/ITMO-WebDev-7-sem/lab_2/static/js/main.331d537c.chunk.js"
+  },
+  {
+    "revision": "e25d08026a5cbc4cba46",
+    "url": "/ITMO-WebDev-7-sem/lab_2/static/js/runtime-main.01c4ba67.js"
+  }
+]);
