dc0f039d18ff37ed8ee94133ef189b5510f33024
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-11-03 23:51:01 +0300

    created a separeate reducer for favorite

--- lab_2/src/store/reducer.js
+++ lab_2/src/store/reducer.js
@@ -1,37 +1,7 @@
-import {FETCH_CITY_LOADING, FETCH_CITY_ERROR, FETCH_CITY_SUCCESS, REMOVE_CITY} from "./actionTypes"
+import { combineReducers } from 'redux';
+import favorites from './reducers/favorites';
 
 
-const reducer = (state = [], action) => {
-
-  switch (action.type) {
-
-    case FETCH_CITY_LOADING:
-      return {
-        favorites: [
-          ...state.favorites,
-          action.payload
-        ]
-      }
-
-    case FETCH_CITY_SUCCESS:
-    case FETCH_CITY_ERROR:
-      return {
-        favorites: state.favorites.map((city, index) => {
-          if (city.name.toLowerCase() === action.payload.name.toLowerCase())
-            return action.payload
-
-          return city
-        })
-      };
-
-    case REMOVE_CITY:
-      return {
-        favorites: state.favorites.filter(city => city.name !== action.payload)
-      };
-
-    default:
-      return state
-  }
-}
-
-export default reducer;
+export default combineReducers({
+  favorites
+})
