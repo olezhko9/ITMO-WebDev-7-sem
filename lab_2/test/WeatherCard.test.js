9d605f850287acb81397fd86010cd2b867232f79
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-11-06 00:29:52 +0300

    created jest setup file

--- lab_2/test/WeatherCard.test.js
+++ lab_2/test/WeatherCard.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
 import { shallow, render } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
 import WeatherCard from '../src/components/WeatherCard';
-
 
 
 describe('Weather Card', () => {
@@ -25,7 +20,5 @@ describe('Weather Card', () => {
       />);
     expect(shallowToJson(tree)).toMatchSnapshot();
   });
-
-
 
 })
