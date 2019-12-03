0f67385398de93c22830bbfa4d3a8738e92d0b82
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-12-03 23:39:26 +0300

    added CRUD commands for favorite sities

--- lab_4/server/routes/favorites.js
+++ lab_4/server/routes/favorites.js
@@ -1,15 +1,30 @@
 const favoritesRouter = require('express').Router()
 
 favoritesRouter.get('/', (req, res) => {
-
+  req.app.locals.db.collection('favorites').find({}).toArray((err, result) => {
+    if (err) return console.log(err);
+    res.status(200).send(result)
+  })
 })
 
 favoritesRouter.post('/', (req, res) => {
-
+  const favoriteCity = {
+    name: req.body.name
+  }
+  req.app.locals.db.collection('favorites').insertOne(favoriteCity, (err, result) => {
+    if (err) return console.log(err);
+    res.status(200).send()
+  })
 })
 
 favoritesRouter.delete('/', (req, res) => {
-
+  const favoriteCity = {
+    name: req.body.name
+  }
+  req.app.locals.db.collection('favorites').deleteOne(favoriteCity, (err, result) => {
+    if (err) return console.log(err);
+    res.status(200).send()
+  })
 })
 
 module.exports = favoritesRouter;
